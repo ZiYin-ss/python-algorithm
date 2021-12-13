2c6fc4249323137a54a81de0452a9f26b2297570
Zhao Qiang <980689346@qq.com>
2021-12-13 11:33:22 +0800

    12/12

--- 算法题/LeetCode/16. 3Sum Closest.py
+++ 算法题/LeetCode/16. 3Sum Closest.py
@@ -5,4 +5,30 @@
 返回这三个数的和。
 假定每组输入只存在恰好一个解。
 """
+"""
+    这道题的解法和15题一模一样 因为你大于target之后你大值不就左移呗(小值右移有意义吗) 
+    不移也不行啊 
+    和15题10题的思想一模一样
+"""
 
+
+class Solution:
+    def threeSumClosest(self, nums, target):
+        nums = sorted(nums)
+        result = nums[0] + nums[1] + nums[len(nums) - 1]
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                val = nums[i] + nums[l] + nums[r]
+                if abs(val - target) <= abs(result - target):
+                    result = val
+                if val == target:
+                    return target
+                elif val < target:
+                    l += 1
+                else:
+                    r -= 1
+        return result
