694110ae7de562cab43badd8baae2290e9b641bd
Zhao Qiang <980689346@qq.com>
2022-01-11 19:00:38 +0800

    1/11

--- 算法题/LeetCode/39. Combination Sum.py
+++ 算法题/LeetCode/39. Combination Sum.py
@@ -6,8 +6,37 @@ candidates 中的 同一个 数字可以 无限制重复被选取 。
 如果至少一个数字的被选数量不同，则两种组合是不同的。
 对于给定的输入，保证和为target 的不同组合数少于 150 个。
 """
+"""
+    这道题有点难 明天再看把 
+"""
 
+class Solution:
+    def combinationSum(self, candidates, target):
+        candidates = sorted(candidates)  # 从小到大排序
 
+        ans = []
 
+        def find(s, use, remain):
+            for i in range(s, len(candidates)):
+                c = candidates[i]
+                if c == remain:
+                    ans.append(use + [c]) # 第一次会走进去四个2 然后 i+1 = 2 不是继续 也不是 一直走到7 返回7 就这意思
+                if c < remain:
+                    find(i, use + [c], remain - c)
+                if c > remain:
+                    return
 
+        find(0, [], target)
 
+        return ans
+
+"""
+    这个题的思想在于你能不能想到重复的找 确实会 现在也不要求 确实难
+"""
+
+candidates = [2, 3, 6, 7]
+target = 7
+
+a = Solution()
+b = a.combinationSum(candidates, target)
+print(b)
